b6266a3603e079d58e0d1e859d477d6a80afbe1a
ArmanSardar <mdarmansardar57@gmail.com>
2024-05-14 10:08:48 +0600

    first Commit

---
+++ Simple Ineractive BankWebsite/bank.js
@@ -0,0 +1,18 @@
+document.getElementById('login-submit').addEventListener('click', function () {
+    const emailfield = document.getElementById(
+        'user-email'
+    )
+    const userEmail = emailfield.value;
+    console.log(userEmail);
+
+
+    const passwordfield = document.getElementById(
+        'user-password'
+    )
+    const userpass = passwordfield.value;
+
+    if (userEmail == 'abc@gmail.com' && userpass == '123') {
+        window.location.href='banking.html'
+    }
+
+})
