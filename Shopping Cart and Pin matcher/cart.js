b52fde9d271073f1c24f62f0664f25aa68110518
ArmanSardar <mdarmansardar57@gmail.com>
2024-05-16 15:41:58 +0600

    shopping cart handel increase decrease use function and update price

--- Shopping Cart and Pin matcher/cart.js
+++ Shopping Cart and Pin matcher/cart.js
@@ -1,9 +1,37 @@
 console.log('js File added');
 
-document.getElementById('plus-button-case').addEventListener('click', function () {
-    const caseQuantity = document.getElementById('case-number')
-    const caseQuantityNumber = caseQuantity.value;
-    caseQuantity.value = parseInt(caseQuantityNumber) + 1
+function updateCaseNumber(button_id, is_plus) {
+    document.getElementById(button_id).addEventListener('click', function () {
+        const caseQuantity = document.getElementById('case-number')
+        let caseQuantityNumber = caseQuantity.value;
+        if (is_plus == true) {caseQuantityNumber  = parseInt(caseQuantityNumber) + 1 }
+        else if (caseQuantityNumber > 0) {
+            caseQuantityNumber  = parseInt(caseQuantityNumber) - 1
+        }
+        caseQuantity.value=caseQuantityNumber
+        const caseTotal = document.getElementById('case-total')
+    
+    caseTotal.innerText = caseQuantityNumber * 59
+    })
+    
+
+}
+increaseCaseNumber = updateCaseNumber('plus-button-case', true)
+decraseCaseNumber = updateCaseNumber('minus-button-case', false)
+
+// document.getElementById('plus-button-case').addEventListener('click', function () {
+//     const caseQuantity = document.getElementById('case-number')
+//     const caseQuantityNumber = caseQuantity.value;
+//     caseQuantity.value = parseInt(caseQuantityNumber) + 1
 
 
-})
+// })
+
+// document.getElementById('minus-button-case').addEventListener('click', function () {
+//     const caseQuantity = document.getElementById('case-number')
+//     const caseQuantityNumber = caseQuantity.value;
+//     caseQuantity.value = parseInt(caseQuantityNumber) - 1
+
+
+// })
+
