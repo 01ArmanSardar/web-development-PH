8b5e22b123adf8ed416c0447641e7084fab0150c
ArmanSardar <mdarmansardar57@gmail.com>
2024-05-17 17:58:07 +0600

    use event bubble to crate calculator and clear

--- Pin Matcher/js/app.js
+++ Pin Matcher/js/app.js
@@ -11,7 +11,23 @@ function getPin() {
 }
 
 function generetPin() {
-    const pin= getPin()
+    const pin = getPin()
     console.log(pin);
-    document.getElementById('Display-Pin').value=pin
+    document.getElementById('Display-Pin').value = pin
 }
+
+document.getElementById('keypad').addEventListener('click', function (event) {
+    const number = event.target.innerText
+    const CalcInput = document.getElementById('typed-numbers');
+    if (isNaN(number)) {
+        if (number == 'C') {
+            CalcInput.value = '';
+        }
+    }
+    else {
+
+        const PreviousNumber = CalcInput.value;
+        const newNumber = PreviousNumber + number
+        CalcInput.value = newNumber
+    }
+})
