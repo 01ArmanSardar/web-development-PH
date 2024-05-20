443b59aec65a80b3baed6b65d27a6e648895d1f6
ArmanSardar <mdarmansardar57@gmail.com>
2024-05-20 23:24:58 +0600

    add div  border, by click using dom event

---
+++ Basic ES6 and ES2015/Dom and Event Handeler(31-2)/dom.js
@@ -0,0 +1,5 @@
+document.getElementById('border-button').addEventListener('click', function(){
+const container=document.getElementById('friend-container')
+container.style.border='3px solid red';
+
+})
