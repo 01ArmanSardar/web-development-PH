195751599cb511a9c3dd2e84115dd5a438ac9e92
ArmanSardar <mdarmansardar57@gmail.com>
2024-05-25 21:16:47 +0600

    big arrow function

--- Basic ES6 and ES2015/arrowFunction.js
+++ Basic ES6 and ES2015/arrowFunction.js
@@ -15,13 +15,24 @@ const sum2 = function (num, num2)
    return num + num2
 }
 
-// arrow Function
+// arrow Function, simple arrow function a bydefault return hoie jai
 const sum3 = (num,num2) => num + num2
+
+// big arrow fucntion er belai { er modeh impliment gulu korteh hoi } abong return korteh hoi 
+const BigarroFunction= (num,num3)=>{
+     
+ const sum=num+num3
+   const minus=sum-num
+   const multiply=minus*2
+   const finalOutput=multiply+2
+   return finalOutput
+}
 
 const result1=  add(15,15)
 const result2= sum(15,15)
 const result3= sum2(15,15)
 const result4= sum3(15,15)
+const result5=BigarroFunction(100,200)
 
 
-console.log(result1,result2,result3,result4);
+console.log(result1,result2,result3,result4,result5);
