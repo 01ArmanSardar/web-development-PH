ba841c9fa3dd9e8c2f8362bd5d89e95fba7ac637
ArmanSardar <mdarmansardar57@gmail.com>
2024-05-25 20:12:38 +0600

    template & dynamic String

---
+++ Basic ES6 and ES2015/template_and_dynamic_string.js
@@ -0,0 +1,11 @@
+const templat_string=`this is a template string ,here we
+write multi line string wihout error faceing
+,normally when we write any multi line string, we use + sign for between 2 string `
+
+console.log(templat_string);
+const price=100
+const quantity=3
+const dynamicString=`this is item Price is ${price}, and we need ${quantity} item ,and 
+total price is ${price*quantity} Dollar `
+
+console.log(dynamicString);
