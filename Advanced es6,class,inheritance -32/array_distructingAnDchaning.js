b0a950b48063317bdc2fc30b2f20cad5e652e19c
ArmanSardar <mdarmansardar57@gmail.com>
2024-05-26 15:44:09 +0600

    array_distructin gAnD chaning

---
+++ Advanced es6,class,inheritance -32/array_distructingAnDchaning.js
@@ -0,0 +1,14 @@
+//array distructing
+
+
+const [first_elemnt,second_element]=[10,20,30]
+// console.log(first_elemnt);
+
+const dummy2 = {
+    name: 'arman', age: 20, address: 'chandpur', phone: '01859136957',
+    hobby: { first_hobby: 'football', second_hobby: 'cricket', third_hobby: 'gaming' }
+}
+
+
+// kono aktah object er property jodhi moneh hoi jeh nah takhe taile amrah ? sign tah use korvho taile ar error asbhe nah undefined dekabhe.
+console.log(dummy2.tobby?.first_hobby);
