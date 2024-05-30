8178f19eea552b6b062367eeadf0c8495edc4c40
ArmanSardar <mdarmansardar57@gmail.com>
2024-05-30 15:31:27 +0600

    dynamically display Loaded Data

--- API/loadDataFormAPi.js
+++ API/loadDataFormAPi.js
@@ -1,15 +1,22 @@
-function loadData(){
+function loadData() {
     fetch('https://jsonplaceholder.typicode.com/todos/1')
-      .then(response => response.json())
-      .then(data => console.log(data))
+        .then(response => response.json())
+        .then(data => console.log(data))
 }
 
-function LoadUsers(){
+function LoadUsers() {
     fetch('https://jsonplaceholder.typicode.com/users')
-    .then(response=>response.json())
-    .then(data=>displayUsers(data))
+        .then(response => response.json())
+        .then(data => displayUsers(data))
 }
 
-function displayUsers(data){
-    console.log(data);
+function displayUsers(data) {
+    const ul = document.getElementById('users')
+    for (const user of data) {
+        console.log(user.name);
+        const li = document.createElement('li')
+        li.innerText=`name : ${user.name}`
+        ul.appendChild(li)
+    }
+
 }
