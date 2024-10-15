31b80f77538676873c5c0716423fe2b50c43815c
ArmanSardar <mdarmansardar57@gmail.com>
2024-10-15 22:39:45 +0600

    Toggle state in based on Click and Conditional Rendering

--- REACT/SimpleReactRestCountries/src/Components/countries/Countries.jsx
+++ REACT/SimpleReactRestCountries/src/Components/countries/Countries.jsx
@@ -1,19 +1,25 @@
 import { useEffect } from "react";
 import { useState } from "react";
 import Country from '../country/Country'
+import './Countries.css'
 const Countries = () => {
     const [countries,setCountires]=useState([])
     useEffect(()=>{
         fetch('https://restcountries.com/v3.1/all')
         .then(res=>res.json())
         .then(data=>setCountires(data))
-    },[])
+    },[]);
+
+   
     return (
         <div>
             <h1>countries : {countries.length}</h1>
+            <div className="Country_Container">
+
             {
                countries.map(country=><Country key={country.cca3} country={country}></Country>)
             }
+            </div>
         </div>
     );
 };
--- REACT/SimpleReactRestCountries/src/Components/country/Country.jsx
+++ REACT/SimpleReactRestCountries/src/Components/country/Country.jsx
@@ -1,12 +1,22 @@
+import { useState } from 'react';
 import './Country.css'
 const Country = ({country}) => {
     const {name,flags}=country
     console.log(country);
+    const [visited,SetVisited]=useState(false)
+    const Visithandeller=()=>
+    {
+        SetVisited(!visited)
+    }
     return (
         <div className='CountryCss'>
 
             <h4>Country: {name.common}</h4>
             <img src={flags.png} alt="flags" />
+            <button onClick={Visithandeller}>{visited?'visitd':'not visitd'}</button>
+            <h5>
+                {visited?'visited ok':'not visited'}
+            </h5>
             
         </div>
     )
