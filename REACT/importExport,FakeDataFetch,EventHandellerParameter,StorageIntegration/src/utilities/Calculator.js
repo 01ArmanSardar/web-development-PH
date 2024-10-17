f866fae2ded8ca7d7a4e7f06c6f5ff21f60c0b14
ArmanSardar <mdarmansardar57@gmail.com>
2024-10-17 11:55:17 +0600

    module export and import

---
+++ REACT/importExport,FakeDataFetch,EventHandellerParameter,StorageIntegration/src/utilities/Calculator.js
@@ -0,0 +1,12 @@
+const add = (num, num2) => {
+    return num + num2;
+}
+
+const multifly = (num, num2) => {
+    return num * num2
+}
+const firstNumberIsDevideBySecondNumberInThisFunction=(num,num2)=>{
+return num/num2
+ }
+// export default add   // export default hisebhe aktai function export korha jai tai niche another way dekano holo 
+export {add,multifly,firstNumberIsDevideBySecondNumberInThisFunction} // jkhn multilple jinish export korteh hobhe tkhn avbhe {function1,function2,.....} object akhare export korteh hobhe ,array akareh oh caile korah jai , ar jodhi sudu akta jinis e export korte hoi tahole export default dilei hobhe
