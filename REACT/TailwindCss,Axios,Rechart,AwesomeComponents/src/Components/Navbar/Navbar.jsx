56badae383d873e9b48d4548764bb26b744cdcd0
ArmanSardar <mdarmansardar57@gmail.com>
2024-10-30 16:09:34 +0600

    create manual responsive navbar,NavbarToggle,NavabrAnimation

--- REACT/TailwindCss,Axios,Rechart,AwesomeComponents/src/Components/Navbar/Navbar.jsx
+++ REACT/TailwindCss,Axios,Rechart,AwesomeComponents/src/Components/Navbar/Navbar.jsx
@@ -1,6 +1,11 @@
+import { useState } from "react";
 import Link from "../NavbarLink/Link";
+// import { IoMenuOutline } from "react-icons/io5";
+import { HiMenuAlt3 } from "react-icons/hi";
+import { IoClose } from "react-icons/io5";
 
 const Navbar = () => {
+    const [open, setOpen] = useState(false)
     const routes = [
         {
             id: 1,
@@ -31,8 +36,15 @@ const Navbar = () => {
 
 
     return (
-        <nav>
-            <ul className="md:flex gap-3">
+        <nav className="bg-yellow-100">
+            <div className="md:hidden" onClick={() => setOpen(!open)}>
+                {
+                    open === true ? < HiMenuAlt3 className="text-2xl " /> : <IoClose className="text-2xl " />
+                }
+            </div>
+
+
+            <ul className={`md:flex gap-3 bg-yellow-500 absolute md:static px-6 ${open ? 'top-16' : '-top-60'}`}>
                 {
                     routes.map(route => <Link key={route.id} route={route}></Link>)
                 }
