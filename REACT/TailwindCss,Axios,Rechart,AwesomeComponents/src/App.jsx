ec8457421a2d552c8ff9a0dedb74553b7d4bfa0a
ArmanSardar <mdarmansardar57@gmail.com>
2024-10-30 12:26:32 +0600

    create manual Navabr using Routes

--- REACT/TailwindCss,Axios,Rechart,AwesomeComponents/src/App.jsx
+++ REACT/TailwindCss,Axios,Rechart,AwesomeComponents/src/App.jsx
@@ -1,13 +1,14 @@
 
 import './App.css'
 import DaisyNav from './Components/DaisyNav/DaisyNav'
-
+import Navbar from './Components/Navbar/Navbar'
 function App() {
  
 
   return (
     <>
-  <DaisyNav></DaisyNav>
+  {/* <DaisyNav></DaisyNav> */}
+<Navbar></Navbar>
     </>
   )
 }
