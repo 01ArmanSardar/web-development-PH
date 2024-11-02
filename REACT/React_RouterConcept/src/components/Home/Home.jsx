5f8790eb8c73e4b0e63b438f9b321f789b064099
ArmanSardar <mdarmansardar57@gmail.com>
2024-11-02 21:34:39 +0600

    Loading Spinner with Use useNavigation()

--- REACT/React_RouterConcept/src/components/Home/Home.jsx
+++ REACT/React_RouterConcept/src/components/Home/Home.jsx
@@ -1,13 +1,16 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigation } from "react-router-dom";
 import Header from "../Header/Header";
 import Fotter from "../Fotter/Fotter";
 const Home = () => {
-
+    const navigation = useNavigation();
     return (
         <div>
             <Header></Header>
-            <Outlet></Outlet>
-            
+            {
+                navigation.state === "loading" ? <p>Loading....</p> : <Outlet></Outlet>
+            }
+
+
             <h2>this is home page</h2>
             <Fotter></Fotter>
         </div>
