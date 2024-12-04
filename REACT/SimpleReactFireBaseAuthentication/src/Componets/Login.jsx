cc24341b13e75b433064ce95556723e4fce218d8
ArmanSardar <mdarmansardar57@gmail.com>
2024-12-04 20:07:28 +0600

    display loggedIn uSerInfo,signOut,toggle sigin Signout button

--- REACT/SimpleReactFireBaseAuthentication/src/Componets/Login.jsx
+++ REACT/SimpleReactFireBaseAuthentication/src/Componets/Login.jsx
@@ -1,19 +1,45 @@
-import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import { getAuth, GoogleAuthProvider, signInWithPopup, signOut } from "firebase/auth";
 import app from "../Firebase/firebase.init";
+import { useState } from "react";
 
 const Login = () => {
+    const [user, setUser] = useState()
     const provider = new GoogleAuthProvider();
     const auth = getAuth(app);
 
     const handelGoogleLogin = () => {
         console.log('click on google login');
         signInWithPopup(auth, provider)
-            .then(result => console.log(result.user))
-            .catch(error=>console.log(error.message))
+            .then(result => {
+                const LogidinUser = result.user
+                setUser(LogidinUser)
+                console.log(LogidinUser);
+            })
+            .catch(error => console.log(error.message))
+    }
+    const handelLogout = () => {
+        signOut(auth)
+            .then(result => {
+                setUser(null)
+                console.log(result)
+            })
+            .catch(error => console.log(error))
     }
     return (
-        <div>
+        <div>{
+            user ?
+            
+            <button className="btn btn-square bg-orange-400" onClick={handelLogout}> Glogout </button>:
             <button className="btn btn-square bg-orange-400" onClick={handelGoogleLogin}>Glogin</button>
+            }
+        
+            {
+                user && <div>
+                    <h1>USer:{user.displayName
+                    }</h1>
+                    <p>Email:{user.email}</p>
+                </div>
+            }
         </div>
     );
 };
