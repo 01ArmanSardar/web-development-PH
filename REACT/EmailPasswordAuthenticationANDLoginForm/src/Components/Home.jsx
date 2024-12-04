82130702d0966f3b3ccf7c5e014b88744ec447fa
ArmanSardar <mdarmansardar57@gmail.com>
2024-12-05 00:07:39 +0600

    Project setup with tailwind,daisyUi,ReactRouter

---
+++ REACT/EmailPasswordAuthenticationANDLoginForm/src/Components/Home.jsx
@@ -0,0 +1,11 @@
+
+
+const Home = () => {
+    return (
+        <div>
+            <h1>this i s home</h1>
+        </div>
+    );
+};
+
+export default Home;
---
+++ REACT/EmailPasswordAuthenticationANDLoginForm/src/main.jsx
@@ -0,0 +1,41 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Main from './Layout/Main';
+import Home from './Components/Home';
+import Header from './Components/Header';
+import Register from './Components/Register';
+import Login from './Components/Login';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Main></Main>,
+    children: [
+      {
+        path: "/",
+        element: <Home></Home>
+      },
+      {
+        path: '/register',
+        element: <Register></Register>
+
+      },
+      {
+        path:'/login',
+        element:<Login/>
+      }
+
+    ]
+  },
+]);
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>,
+)
