80fecbf6db23086873960776b37a450fdebf8498
ArmanSardar <mdarmansardar57@gmail.com>
2024-12-05 11:59:25 +0600

    create user using email and password and login \(firebase)

--- REACT/EmailPasswordAuthenticationANDLoginForm/src/Components/Login.jsx
+++ REACT/EmailPasswordAuthenticationANDLoginForm/src/Components/Login.jsx
@@ -1,9 +1,67 @@
-import React from 'react';
+import { getAuth, signInWithEmailAndPassword, } from "firebase/auth";
+import app from "../FireBase/Firebase.init";
+import { useState } from "react";
 
 const Login = () => {
+    const [user, setUser] = useState(null)
+    const HandelRegister = (e) => {
+        e.preventDefault();
+        const email = e.target.email.value;
+        const password = e.target.password.value;
+        console.log("click in login form");
+        console.log(email, password);
+
+        //
+        const auth = getAuth(app)
+        signInWithEmailAndPassword(auth, email, password)
+            .then(result => {
+                const Luser = result.user
+                setUser(Luser)
+                console.log('done login');
+            }
+
+            )
+            .catch(error => {
+                console.log(error.message);
+            })
+    }
+
+
     return (
-        <div>
-            <h1>this is lgin page</h1>
+        <div className="hero bg-base-200 min-h-screen">
+            <div className="hero-content flex-col lg:flex-row-reverse">
+                <div className="text-center lg:text-left">
+                    <h1 className="text-5xl font-bold">Login</h1>
+                    <p className="py-6">
+                        Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
+                        quasi. In deleniti eaque aut repudiandae et a id nisi.
+                    </p>
+                </div>
+                <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
+                    <form onSubmit={HandelRegister} className="card-body">
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Email</span>
+                            </label>
+                            <input name="email" type="email" placeholder="email" className="input input-bordered" required />
+                        </div>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Password</span>
+                            </label>
+                            <input name="password" type="password" placeholder="password" className="input input-bordered" required />
+                            <label className="label">
+                                <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
+                            </label>
+                        </div>
+                        <div className="form-control mt-6">
+                            <button className="btn btn-primary">Login</button>
+                        </div>
+                    </form>
+
+                    {/* <h1>User:{user.displayName}</h1> */}
+                </div>
+            </div>
         </div>
     );
 };
---
+++ REACT/EmailPasswordAuthenticationANDLoginForm/src/FireBase/Firebase.init.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyA0y-2m4wU1019A884o37yNulhQ191fUJc",
+  authDomain: "email-password-authentic-fb9fa.firebaseapp.com",
+  projectId: "email-password-authentic-fb9fa",
+  storageBucket: "email-password-authentic-fb9fa.firebasestorage.app",
+  messagingSenderId: "29182526299",
+  appId: "1:29182526299:web:0d266a72d176e2b0b6107e"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app
