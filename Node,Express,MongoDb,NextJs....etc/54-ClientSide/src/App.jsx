a91029f08b5820551ca23c813c2640aba2599da1
ArmanSardar <mdarmansardar57@gmail.com>
2025-01-12 14:30:16 +0600

    send client data in server side

--- Node,Express,MongoDb,NextJs....etc/54-ClientSide/src/App.jsx
+++ Node,Express,MongoDb,NextJs....etc/54-ClientSide/src/App.jsx
@@ -9,12 +9,46 @@ function App() {
       .then(res => res.json())
       .then(data => setUSers(data))
   }, [])
+  const handelUserForm = (e) => {
 
+    e.preventDefault();
+    const form = e.target
+    const name = form.name.value
+    const email = form.email.value
+    console.log(name, email, 'in handeluserForm');
+    const user = { name, email }
+    fetch('http://localhost:5000/users', {
+      method: "POST",
+      headers: {
+        'content-type': 'application/json'
+      },
+      body: JSON.stringify(user)
+    })
+      .then(res => res.json())
+      .then(data => {
+        console.log(data);
+        const newUSers = [...users, data]
+        setUSers(newUSers)
+        form.reset()
+      }
+
+      )
+  }
   return (
+
     <>
 
       <h1>this is second server client side</h1>
       <h4>lentgh of users is {users.length}</h4>
+      <form onSubmit={handelUserForm} >
+
+        <input type="text" name='name' placeholder='name' />
+        <input type="email" name='email' placeholder='email' />
+        <input type="submit" value="submit" />
+      </form>
+      {
+        users.map(user => <p key={user.id}>{user.name} {user.email}</p>)
+      }
     </>
   )
 }
