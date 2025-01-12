042dd02eddfd26f7d855fb3dfa40d5d0b305697c
ArmanSardar <mdarmansardar57@gmail.com>
2025-01-12 14:31:03 +0600

    receve client data by post request

--- Node,Express,MongoDb,NextJs....etc/54-ServerSide/index.js
+++ Node,Express,MongoDb,NextJs....etc/54-ServerSide/index.js
@@ -3,10 +3,11 @@ const app = express()
 const port = process.env.PORT || 5000;
 const cors =require('cors')
 app.use(cors())
+app.use(express.json());
 const users = [
-    { name: 'arman', age: 78, roll: 90 },
-    { name: 'arsu', age: 8, roll: 40 },
-    { name: 'arba', age: 7, roll: 103 }
+    {id:1, name: 'arman', age: 78, roll: 90 },
+    {id:2, name: 'arsu', age: 8, roll: 40 },
+    {id:3, name: 'arba', age: 7, roll: 103 }
 ]
 app.get('/', (req, res) => {
     res.send('this is second server')
@@ -17,6 +18,17 @@ app.get('/users', (req, res) => {
     res.send(users)
 })
 
+app.post('/users',(req,res)=>{
+    console.log('users from react hitting');
+    console.log(req.body);
+    const Newuser=req.body
+    Newuser.id=users.length+1
+    users.push(Newuser)
+    console.log(users);
+    res.send(Newuser)
+})
+
 app.listen(port, () => {
     console.log(`this server runing on${port}`);
 })
+
