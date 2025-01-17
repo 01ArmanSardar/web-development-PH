6ebe629427d32a2bc84a035c9273140ac6db36df
ArmanSardar <mdarmansardar57@gmail.com>
2025-01-17 18:31:56 +0600

    create form and add coffe and send database

--- Node,Express,MongoDb,NextJs....etc/56-Coffe-store-espresso-emporium-server/index.js
+++ Node,Express,MongoDb,NextJs....etc/56-Coffe-store-espresso-emporium-server/index.js
@@ -1,15 +1,15 @@
-const express =require('express')
-const cors=require('cors')
+const express = require('express')
+const cors = require('cors')
 require('dotenv').config()
 const { MongoClient, ServerApiVersion } = require('mongodb');
-const app=express()
-const port =process.env.port || 5000;
+const app = express()
+const port = process.env.port || 5000;
 
 //midelware
 app.use(cors())
 app.use(express.json())
 
-const uri=process.env.URI
+const uri = process.env.URI
 
 
 
@@ -26,27 +26,41 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
-    const database = client.db("COffeDb");
-    const coffeCollection = database.collection("CofffeList");
-    const doc = {
-        title: "BestCoffe701",
-        content: "this is good cofffe okkkkkkkkkkk",
-      }
-      const result = await coffeCollection.insertOne(doc);
+
+    // const database = client.db("COffeDb");
+    // const coffeCollection = database.collection("CofffeList");
+    // const doc = {
+    //     title: "BestCoffe701",
+    //     content: "this is good cofffe okkkkkkkkkkk",
+    //   }
+    //   const result = await coffeCollection.insertOne(doc);
+    app.post('/coffe', async (req, res) => {
+
+      const Newcoffe = req.body
+      const Coffedatabase = client.db("1Coffedb");
+      const CoffeCollection = Coffedatabase.collection("CofffeCollection");
+      console.log(Newcoffe, 'in index.js file coffe added api');
+
+      const result = await CoffeCollection.insertOne(Newcoffe);
+      res.send(result)
+
+    })
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } catch (err){
-    console.log('error occure ',err);
+  } catch (err) {
+    console.log('error occure ', err);
   }
 }
 run().catch(console.dir);
 
 
-app.get('/',(req,res)=>{
-    res.send('coffe server is done ')
+app.get('/', (req, res) => {
+  res.send('coffe server is done ')
 })
 
-app.listen(port,()=>{
-    console.log(`this server open in this port ${port}`);
+app.listen(port, () => {
+  console.log(`this server open in this port ${port}`);
 })
