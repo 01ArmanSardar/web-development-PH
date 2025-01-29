64515076cd687d12955349df3706f5ea659059ca
ArmanSardar <mdarmansardar57@gmail.com>
2025-01-29 16:50:14 +0600

    basic setup of server and database and get data from database

---
+++ Node,Express,MongoDb,NextJs....etc/58-JobPortalServer/index.js
@@ -0,0 +1,59 @@
+const express = require('express')
+const cors = require('cors')
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const port = process.env.PORT || 3000
+require('dotenv').config()
+
+const app = express()
+app.use(cors())
+app.use(express.json())
+
+
+
+
+
+const uri = process.env.URI;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
+
+        const JobsCollection = client.db('JobPortal').collection('Jobs')
+
+        app.get('/jobs', async (req, res) => {
+            const cursor =JobsCollection.find()
+            const result=await cursor.toArray()
+            res.send(result)
+        })
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    }
+    catch (err) {
+        console.log(err);
+    }
+    // finally {
+    //     // Ensures that the client will close when you finish/error
+    //     await client.close();
+    // }
+}
+run().catch(console.dir);
+
+
+app.get('/', (req, res) => {
+    res.send('this is jobPortal server Home page')
+})
+
+app.listen(port, () => {
+    console.log(`this server open on this port ${port}`);
+})
