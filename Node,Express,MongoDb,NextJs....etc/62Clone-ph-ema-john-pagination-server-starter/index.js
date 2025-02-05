c1a5f3210e5c8723a8fd55a889e633875c33edf0
ArmanSardar <mdarmansardar57@gmail.com>
2025-02-05 19:48:58 +0600

    impliment post api for load some data by products id

--- Node,Express,MongoDb,NextJs....etc/62Clone-ph-ema-john-pagination-server-starter/index.js
+++ Node,Express,MongoDb,NextJs....etc/62Clone-ph-ema-john-pagination-server-starter/index.js
@@ -11,7 +11,7 @@ app.use(express.json());
 
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = process.env.URI
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -31,8 +31,34 @@ async function run() {
     const productCollection = client.db('emaJohnDB').collection('products');
 
     app.get('/products', async (req, res) => {
-      const result = await productCollection.find().toArray();
+      // const query = req.query
+      // console.log(query);
+      const page = parseInt(req.query.page)
+      const size = parseInt(req.query.size)
+      const result = await productCollection.find().skip(page * size)
+        .limit(size)
+        .toArray()
+
       res.send(result);
+    })
+    app.post('/productsByIds', async (req, res) => {
+      const ids = req.body
+      console.log(ids);
+      const idsWithObjectID = ids.map(id => new ObjectId(id))
+      const query = {
+        _id: {
+          $in: idsWithObjectID
+        }
+      }
+      const result = await productCollection.find(query).toArray()
+
+      res.send(result)
+    })
+    app.get('/productCounts', async (req, res) => {
+      const count = await productCollection.estimatedDocumentCount()
+      console.log(count);
+      res.send({ count })
+
     })
 
     // Send a ping to confirm a successful connection
