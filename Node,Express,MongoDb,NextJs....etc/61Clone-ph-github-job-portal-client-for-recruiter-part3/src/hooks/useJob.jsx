411e28403f4c9acee5af517cd61770991e5b0009
ArmanSardar <mdarmansardar57@gmail.com>
2025-02-06 12:38:48 +0600

    search job by min and max salary

--- Node,Express,MongoDb,NextJs....etc/61Clone-ph-github-job-portal-client-for-recruiter-part3/src/hooks/useJob.jsx
+++ Node,Express,MongoDb,NextJs....etc/61Clone-ph-github-job-portal-client-for-recruiter-part3/src/hooks/useJob.jsx
@@ -2,11 +2,11 @@ import React, { useEffect, useState } from 'react';
 import useAuth from './useAuth';
 import axios from 'axios';
 
-const useJob = (sort,search) => {
+const useJob = (sort, search, minSalary, maxSalary) => {
     const { loading, setLoading } = useAuth()
     const [jobs, setjobs] = useState([])
     useEffect(() => {
-        axios.get(`http://localhost:5000/jobs?sort=${sort}&search=${search}`)
+        axios.get(`http://localhost:5000/jobs?sort=${sort}&search=${search}&min=${minSalary}&max=${maxSalary}`)
             .then((res) => {
 
                 setjobs(res.data)
@@ -15,7 +15,7 @@ const useJob = (sort,search) => {
 
             })
 
-    }, [sort,search])
+    }, [sort, search,minSalary,maxSalary])
 
     return { jobs }
 };
--- Node,Express,MongoDb,NextJs....etc/61Clone-ph-github-job-portal-client-for-recruiter-part3/src/pages/AllJobs/AllJobs.jsx
+++ Node,Express,MongoDb,NextJs....etc/61Clone-ph-github-job-portal-client-for-recruiter-part3/src/pages/AllJobs/AllJobs.jsx
@@ -5,15 +5,18 @@ import HotJobCard from "../Home/HotJobCard";
 
 
 const AllJobs = () => {
-    const [sort,Setsort]=useState(false)
-    const [search,SetSearch]=useState('')
-    const { jobs } = useJob(sort,search)
-    console.log(jobs);
-    console.log(search);
-    
+    const [sort, Setsort] = useState(false)
+    const [search, SetSearch] = useState('')
+    const [minSalary, SetMinSalary] = useState('')
+    const [maxSalary, SetMaxSalary] = useState('')
+    const { jobs } = useJob(sort, search,minSalary,maxSalary)
+    // console.log(jobs);
+    // console.log(search);
+    console.log(minSalary,maxSalary);
+
     console.log(sort);
     const { loading } = useAuth()
-    if (loading){
+    if (loading) {
         return <p className="text-9xl font-bold text-red-900">job is loading</p>
     }
     return (
@@ -28,14 +31,19 @@ const AllJobs = () => {
             } */}
             <h1>All jobs here</h1>
             <div className="bg-base-200 flex items-center gap-3">
-            <button onClick={()=>Setsort(!sort)} className={sort? `btn btn-success`: `btn btn-primary`}>{sort?`sorted by salary`:`Sort by Salary`}</button>
-           <input onKeyUp={(e)=>SetSearch(e.target.value)} type="text"  placeholder="search by location is here"/>
-            </div>
-                <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4'>
-                    {
-                        jobs.map(job => <HotJobCard key={job._id} job={job}></HotJobCard>)
-                    }
+                <button onClick={() => Setsort(!sort)} className={sort ? `btn btn-success` : `btn btn-primary`}>{sort ? `sorted by salary` : `Sort by Salary`}</button>
+                <input onKeyUp={(e) => SetSearch(e.target.value)} type="text" placeholder="search by location is here" />
+
+                <div className="">
+                    <input className="m-4" onKeyUp={(e) => SetMinSalary(e.target.value)} type="text" placeholder="set min salary" />
+                    <input className="m-4" onKeyUp={(e) => SetMaxSalary(e.target.value)} type="text" placeholder="set max salary" />
                 </div>
+            </div>
+            <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4'>
+                {
+                    jobs.map(job => <HotJobCard key={job._id} job={job}></HotJobCard>)
+                }
+            </div>
         </div>
     );
 };
